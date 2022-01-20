75274a78bbd0f039871348c12f4b9d95140a912a
phuongnguyenanh1504 <phuong.misadng@gmail.com>
2022-01-20 07:56:48 +0700

    up ss9

---
+++ module1/ss9_cau_truc_dieu_kien_2/[Bài tập] Máy tính đơn giản/maytinhcode.js
@@ -0,0 +1,33 @@
+function laygiatricu() {
+    return document.getElementById('xuat_ket_qua').innerText;
+}
+function xuatketqua(so) {
+    document.getElementById('xuat_ket_qua').innerText = so;
+}
+
+var he_thong=document.getElementsByClassName('he_thong');
+for (var i=0;i<he_thong.length;i++) {
+    he_thong[i].addEventListener("click",function (){
+        if (this.id=="xoa_tat_ca") {
+            xuatketqua("")
+        }
+        else {
+            if (this.id=="=") {
+                ket_qua=eval(laygiatricu())
+                xuatketqua(ket_qua)
+            }
+            else {
+                ket_qua=laygiatricu()+this.id;
+                xuatketqua(ket_qua)
+            }
+        }
+
+    })
+}
+var con_so=document.getElementsByClassName('con_so');
+for (var i=0;i<con_so.length;i++) {
+    con_so[i].addEventListener('click',function (){
+        ket_qua=laygiatricu()+Number(this.id);
+        xuatketqua(ket_qua)
+    })
+}
