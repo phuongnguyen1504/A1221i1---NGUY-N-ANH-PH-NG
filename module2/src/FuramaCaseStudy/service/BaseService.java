2c2e76d15b4aa94d46264608b4e842e6e0abb52c
phuongnguyenanh1504 <phuong.misadng@gmail.com>
2022-05-10 22:03:58 +0700

    up

--- module2/src/FuramaCaseStudy/service/BaseService.java
+++
@@ -1,8 +0,0 @@
-package FuramaCaseStudy.service;
-
-import java.util.List;
-
-public interface BaseService <T>{
-    void save(T t);
-    List<T> getAll();
-}
--- module2/src/FuramaCaseStudy/service/EmployeeService.java
+++
@@ -1,6 +0,0 @@
-package FuramaCaseStudy.service;
-
-import FuramaCaseStudy.model.Employee;
-
-public interface EmployeeService extends BaseService<Employee> {
-}
