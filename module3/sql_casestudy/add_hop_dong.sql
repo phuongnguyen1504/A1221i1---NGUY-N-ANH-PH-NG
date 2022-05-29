007d363adc9fe2d692734a9884b7f6a908abcb88
phuongnguyenanh1504 <96730058+phuongnguyenanh1504@users.noreply.github.com>
2022-05-29 15:23:22 +0700

    up
    up

---
+++ module3/sql_casestudy/add_hop_dong.sql
@@ -0,0 +1,5 @@
+SELECT * FROM furama_database.hop_dong;
+INSERT INTO `furama_database`.`hop_dong` (`ma_hop_dong`, `ngay_lam_hop_dong`, `ngay_ket_thuc`, `tien_dat_coc`, `ma_nhan_vien`, `ma_khach_hang`, `ma_dich_vu`) VALUES ('1', '2021-12-31', '2022-02-28', '8000000', '1', '1', '1');
+INSERT INTO `furama_database`.`hop_dong` (`ma_hop_dong`, `ngay_lam_hop_dong`, `ngay_ket_thuc`, `tien_dat_coc`, `ma_nhan_vien`, `ma_khach_hang`, `ma_dich_vu`) VALUES ('2', '2022-03-15', '2022-05-15', '4000000', '3', '3', '2');
+INSERT INTO `furama_database`.`hop_dong` (`ma_hop_dong`, `ngay_lam_hop_dong`, `ngay_ket_thuc`, `tien_dat_coc`, `ma_nhan_vien`, `ma_khach_hang`, `ma_dich_vu`) VALUES ('3', '2022-04-01', '2022-05-30', '1000000', '2', '4', '3');
+INSERT INTO `furama_database`.`hop_dong` (`ma_hop_dong`, `ngay_lam_hop_dong`, `ngay_ket_thuc`, `tien_dat_coc`, `ma_nhan_vien`, `ma_khach_hang`, `ma_dich_vu`) VALUES ('4', '2022-01-31', '2022-06-01', '5000000', '5', '2', '4');
---
+++ module3/sql_casestudy/add_hop_dong_chi_tiet.sql
@@ -0,0 +1,5 @@
+SELECT * FROM furama_database.hop_dong_chi_tiet;
+INSERT INTO `furama_database`.`hop_dong_chi_tiet` (`ma_hop_dong_chi_tiet`, `ma_hop_dong`, `ma_dich_vu_di_kem`, `so_luong`) VALUES ('1', '1', '1', '4');
+INSERT INTO `furama_database`.`hop_dong_chi_tiet` (`ma_hop_dong_chi_tiet`, `ma_hop_dong`, `ma_dich_vu_di_kem`, `so_luong`) VALUES ('2', '2', '2', '3');
+INSERT INTO `furama_database`.`hop_dong_chi_tiet` (`ma_hop_dong_chi_tiet`, `ma_hop_dong`, `ma_dich_vu_di_kem`, `so_luong`) VALUES ('3', '3', '5', '2');
+INSERT INTO `furama_database`.`hop_dong_chi_tiet` (`ma_hop_dong_chi_tiet`, `ma_hop_dong`, `ma_dich_vu_di_kem`, `so_luong`) VALUES ('4', '4', '3', '1');
