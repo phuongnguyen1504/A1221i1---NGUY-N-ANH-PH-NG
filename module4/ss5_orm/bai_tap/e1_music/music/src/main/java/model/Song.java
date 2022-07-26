878fd882ab9f8ea4dde551ba247637196ceadc61
phuongnguyenanh1504 <phuong.misadng@gmail.com>
2022-07-26 22:17:08 +0700

    up

--- module4/ss5_orm/bai_tap/e1_music/music/src/main/java/model/Song.java
+++ module4/ss5_orm/bai_tap/e1_music/music/src/main/java/model/Song.java
@@ -3,7 +3,7 @@ package model;
 import jakarta.persistence.*;
 
 @Entity
-@Table
+@Table(name = "Song")
 public class Song {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- module4/ss5_orm/bai_tap/e1_music/music/src/main/java/service/SongService.java
+++ module4/ss5_orm/bai_tap/e1_music/music/src/main/java/service/SongService.java
@@ -1,5 +1,8 @@
 package service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import model.Song;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,12 @@ import java.util.List;
 @Service
 public class SongService {
     private List<Song> songs=new ArrayList<>();
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public List<Song> findAll() {
-
+        TypedQuery<Song> query = entityManager
+                .createQuery("SELECT s FROM Song as s", Song.class);
+        return query.getResultList();
     }
 }
