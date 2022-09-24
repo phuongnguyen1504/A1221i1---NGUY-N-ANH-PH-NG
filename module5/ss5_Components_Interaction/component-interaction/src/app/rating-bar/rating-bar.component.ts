c45858b08b621109b1b03bbcfd5afb76ae1dfcde
phuongnguyenanh1504 <96730058+phuongnguyenanh1504@users.noreply.github.com>
2022-09-24 19:44:49 +0700

    up

---
+++ module5/ss5_Components_Interaction/component-interaction/src/app/rating-bar/rating-bar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {IRatingUnit} from "../irating-unit";
+@Component({
+  selector: 'app-rating-bar',
+  templateUrl: './rating-bar.component.html',
+  styleUrls: ['./rating-bar.component.css']
+})
+export class RatingBarComponent implements OnInit {
+  ratings:IRatingUnit[]=[];
+  rating:IRatingUnit = {value : 1, active: true};
+  constructor() {
+    for (let i=1;i<=10;i++){
+      const  obj1: IRatingUnit = { value: i, active: false };
+      this.ratings.push(obj1);
+      console.log(obj1);
+    }
+
+    // this.ratings.forEach(e => {
+    //   console.log("val " +e.value)
+    // })
+  }
+
+  ngOnInit(): void {
+  }
+
+  vote(rating: IRatingUnit) {
+    // this.value=rating.value;
+    // for (let item of this.ratings){
+    //   if (item.value<=this.value){
+    //     item.active=true;
+    //   }
+    //   else {
+    //     item.active=false;
+    //   }
+    // }
+  }
+}
